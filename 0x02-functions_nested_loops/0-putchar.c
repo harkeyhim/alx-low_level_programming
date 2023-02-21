9a6d7d35c0d8dfb59943da13ff8da0f829dc04f3
harkeyhim <harkeyhim@gmail.com>
2023-02-21 11:48:02 -0800

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include  "MAIN.H"
+/**
+ * main -  Entry point
+ *
+ * Description: A program that print a _putchar
+ *
+ * Return: Always 0 (success)
+*/
+int main(void) 
+{
+	putchar('_');
+	putchar('p');		
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return(0);
+}
+
