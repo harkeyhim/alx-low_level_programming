327b62f9c0845c9992e702a8e5010ad7620e9637
harkeyhim <harkeyhim@gmail.com>
2023-02-21 13:44:22 -0800

    message

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * _islower - Entry point
+ *
+ *Description:  A functions that checks for lowercase characters
+ *
+ * Return : Always 0(success)
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
